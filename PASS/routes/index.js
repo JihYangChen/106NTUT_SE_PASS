dfe78f541bcbc50d2e5f2d1754b29ffb93551462
AndyChen <andy565866@gmail.com>
2017-12-14 20:16:47 +0800

    Add login page.

--- PASS/routes/index.js
+++ PASS/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/course', function(req, res, next) {
+  res.render('course', { title: 'Express' });
+});
+
 module.exports = router;
