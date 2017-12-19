0e2699aab08bb21401b8871c75f6a153ef237f13
AndyChen <andy565866@gmail.com>
2017-12-19 20:48:43 +0800

    Add course schema.

--- PASS/lib/db.js
+++ PASS/lib/db.js
@@ -8,13 +8,24 @@ var userSchema = new Schema({
     email : { type : String, required : true },
     tel : { type : String },
     website : { type : String },
-    classid : { type: Schema.Types.ObjectId, ref:'department' },
-    userType : { type: String, required: true }
+    classid : { type: String, ref: 'department' },
+    userType : { type: String, required: true },
+    courses: { type: [String], ref: 'course' }
 }, { collection: 'user' });
 
 var departmentSchema = new Schema({
+    id : { type : String, required : true }, 
     name : { type : String, required : true }
 },  { collection: 'department' });
+
+var courseSchema = new Schema({
+    id : { type : String, required : true }, 
+    name : { type : String, required : true },
+    classid : {type : String, required : true },
+    teacherAccount : {type : String, required : true },
+    TA : {type : [String], ref: 'user' },
+    studentAccount : {type : [String], ref: 'user' },
+},  { collection: 'course' });
 
 mongoose.model('user', userSchema);
 mongoose.model('department', departmentSchema);
@@ -23,6 +34,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://passadmin:1421@ds159737.mlab.com:59737/pass', {
     useMongoClient: true
 });
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Conncet database fail.'));
 db.once('open', function callback() {
